b70795ec32729808536b82f7ad01dbcb85f120f3
rail888 <belokana@hanmail.net>
2026-08-05 16:23:54 +0900

    처음 올려봅니다.

---
+++ src/main/java/pack/OrderService02Application.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrderService02Application {
+	@GetMapping("/")
+	public String home() {
+		System.out.println("하이");
+		return "16:00";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(OrderService02Application.class, args);
+	}
+
+}
